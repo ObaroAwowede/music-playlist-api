f8c3c0846c764d637c3305148e43461140644c58
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-12 12:33:39 +0100

    Created models, for user, song, arist, album and playlist

--- playlists/models.py
+++ playlists/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 100)
+    email = models.EmailField(max_length = 254)
+
+class Artist(models.Model):
+    name = models.CharField(max_length = 100)
+    bio = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 100)
+    release_date = models.DateField()
+    album_length = models.TimeField()
+    genre = models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 100)
+    genre = models.TextField()
+    duration = models.TimeField()
+    
+    def __str__(self):
+        return self.title
+
+class Playlist(models.Model):
+    title = models.CharField(max_length = 100)
+    song = models.ManyToManyField(Song)
+    description = models.TextField()
+    size = models.IntegerField(default = 0)
+    
+    def __str__(self):
+        return self.title
