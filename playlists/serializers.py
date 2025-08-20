1d115eb9842b9cf6645260b69547eba013ad46db
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-20 15:37:03 +0100

    Updated serializers, making owner field read-only

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -6,21 +6,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
-        
-    
-
 
 class AlbumSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
     class Meta:
         model = Album
         fields = '__all__'
 
 class SongSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
     class Meta:
         model = Song
         fields = '__all__'
         
 class ArtistSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
     class Meta:
         model = Artist
         fields = '__all__'
@@ -31,13 +31,13 @@ class GenreSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class PlaylistSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True) 
+    owner = UserSerializer(read_only=True)
     song_count = serializers.SerializerMethodField()
     size = song_count
     songs = SongSerializer(many=True, read_only=True)
     class Meta:
         model = Playlist
-        fields = ('title','songs','description','user','song_count')
+        fields = ('title','songs','description','owner','song_count')
         
     def get_song_count(self, obj):
         return obj.songs.count()
