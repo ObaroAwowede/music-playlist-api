7834217d1449031b93d64bb5900f3ca77b062157
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-28 13:14:03 +0100

    Created register serializer to handle registration of users (I'm not creating tokens here for each new user, since i used jwt and already have an endpoint for retrieving tokens)

--- playlists/serializers.py
+++ playlists/serializers.py
@@ -48,3 +48,16 @@ class PlaylistSerializer(serializers.ModelSerializer):
     def get_song_count(self, obj):
         return obj.songs.count()
     
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only = True)
+    class Meta:
+        model: User
+        fields = ['id','username', 'email', 'password']
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username= validated_data['username'],
+            password=validated_data['password '],
+            email=validated_data.get('email')
+        )
+        return user
