c512dc5ff6e66e55e3d62a28dc65d99cc21d7ccf
Obaro Awowede <akpobaroawowede@gmail.com>
2025-08-29 13:59:51 +0100

    Preparing for deployment

--- music_playlist_api/settings.py
+++ music_playlist_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -20,13 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ct7u2u8ysv-nq!qhiop29(jtuxlr1))3qe52wu6fq92tx8y6kd'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-local-secret-key')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'ManLikeObaro.pythonanywhere.com').split(',')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
